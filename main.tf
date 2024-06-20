9e4c9fe6fe570a263c7aed092ee092c3efd0712f
bhanu prakash <108963598+Bhanu1616@users.noreply.github.com>
2024-06-20 16:30:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_iinstance" {
   ami             = "ami-0eaf7c3456e7b5b68"
   instance_type   = "t2.micro"
-  key_name        = "i"
+  key_name        = "l"
   subnet_id       = aws_subnet.my_subnet_1.id
   security_groups = [aws_security_group.my_sg.id]
   root_block_device {
